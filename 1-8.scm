c37b54b61fc02d9368df05214cc4a23ae19eb049
giggi <hiromichi@rx7fc.com>
2010-09-04 09:38:15 +0900

    resolved 1-8.scm

--- 1-8.scm
+++ 1-8.scm
@@ -1,10 +1,15 @@
-; 復習で Newton Square
+;
+; sqrt
+;
 
+(define (abs x)
+  (if (< x 0) (- 0 x) x))
 (define (square x)
-  (define (abs x)
-    (if (< x 0) (- 0 x) x))
-  (define (square x)
-    (* x x))
+  (* x x))
+(define (cube x)
+  (* x x x))
+
+(define (sqrt x)
   (define (average x y)
     (/ (+ x y) 2))
   (define (good-enough? guess x)
@@ -17,4 +22,17 @@
         (square-iter (improve guess x) x)))
   (square-iter 1.0 x))
 
-(define (cube x)
+;
+; cubic root
+;
+
+(define (cubic-root x)
+  (define (improve guess x)
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+  (define (good-enough? guess x)
+    (< (abs (- (cube guess) x)) 0.001))
+  (define (cubic-root-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (cubic-root-iter (improve guess x) x)))
+  (cubic-root-iter 1.0 x))
